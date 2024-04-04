95d63fd42bbe233a2e22e64d0dabfbb261c682fb
shaiurbekuO <shaiurbekuuluomurbek@gmail.com>
2024-04-04 20:32:19 +0600

    Задача на арифметические операции

---
+++ Задачи-на-арифметические-операции.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+
+        double c = Math.sqrt(a * a + b * b);
+
+        System.out.println(c);
+    }
+}
