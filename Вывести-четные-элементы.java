691c9da12f022df1bbbe874aedbb948a61bfeaf9
shaiurbekuO <shaiurbekuuluomurbek@gmail.com>
2024-05-14 12:44:05 +0600

    Вывести четные элементы

---
+++ Вывести-четные-элементы.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class MyProgram{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        for(int i=0; i<n; i++){
+            array[i] = sc.nextInt();
+        }
+        for(int i=0; i<n; i++){
+            if(array[i]%2==0){
+                System.out.print(array[i]+" ");}
+        }
+    }
+}
